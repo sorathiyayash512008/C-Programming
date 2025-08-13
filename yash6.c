0368f396422d9407e0d345512ce15c73f147686d
sorathiyayash512008 <sorathiyayash512008@gmail.com>
2025-08-13 11:29:04 +0530

    Add files via upload

---
+++ yash6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int hours,minutes;
+    
+    printf("Enter the number of hours :");
+    scanf("%d",&hours);
+    
+    minutes=hours*60;
+    
+    printf("conversion of hours into minutes is = %d", minutes);
+    
+    
+    return 0;
+    
+}
+
+
+
